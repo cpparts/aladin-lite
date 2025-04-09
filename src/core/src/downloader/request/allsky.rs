cpparts/aladin-lite

commit ee2eb6e704ee55902c395469ff923f125ae42480

Fix NaN detection by the GPU on float textures for window platform
float textures coming from BITPIX<0 fits images are sent to GPU as
RGBA8UI textures. Float decoding from a vec4 rgba is done in the shader.
Then this obtained decoded float can be tested against nan/inf in the
shader

src/core/src/downloader/request/allsky.rs
@@ -160,47 +160,42 @@ impl From<query::Allsky> for AllskyRequest {
                     let Fits { hdu } = Fits::from_reader(&mut reader)
                         .map_err(|_| JsValue::from_str("Parsing fits error of allsky"))?;
 
-                    //let width_allsky_px = 27 * std::cmp::min(tile_size, 64) as i32;
-                    //let height_allsky_px = 29 * std::cmp::min(tile_size, 64) as i32;
                     let data = hdu.get_data();
 
                     match data {
                         InMemData::U8(data) => {
                             Ok(handle_allsky_fits(&data, tile_size, texture_size)?
-                                .into_iter()
                                 .map(|image| ImageType::RawR8ui { image })
                                 .collect())
                         }
                         InMemData::I16(data) => {
                             Ok(handle_allsky_fits(&data, tile_size, texture_size)?
-                                .into_iter()
                                 .map(|image| ImageType::RawR16i { image })
                                 .collect())
                         }
                         InMemData::I32(data) => {
                             Ok(handle_allsky_fits(&data, tile_size, texture_size)?
-                                .into_iter()
                                 .map(|image| ImageType::RawR32i { image })
-                                .collect())
-                        }
-                        InMemData::F32(data) => {
-                            Ok(handle_allsky_fits(&data, tile_size, texture_size)?
-                                .into_iter()
-                                .map(|image| ImageType::RawR32f { image })
                                 .collect())
                         }
                         InMemData::I64(data) => {
                             let data = data.iter().map(|v| *v as i32).collect::<Vec<_>>();
                             Ok(handle_allsky_fits(&data, tile_size, texture_size)?
-                                .into_iter()
                                 .map(|image| ImageType::RawR32i { image })
+                                .collect())
+                        }
+                        InMemData::F32(data) => {
+                            let data = unsafe { std::slice::from_raw_parts(data.as_ptr() as *const u8, data.len() * 4) };
+                            Ok(handle_allsky_fits(&data, tile_size, texture_size)?
+                                .map(|image| ImageType::RawRgba8u { image })
                                 .collect())
                         }
                         InMemData::F64(data) => {
                             let data = data.iter().map(|v| *v as f32).collect::<Vec<_>>();
+                            let data = unsafe { std::slice::from_raw_parts(data.as_ptr() as *const u8, data.len() * 4) };
+
                             Ok(handle_allsky_fits(&data, tile_size, texture_size)?
-                                .into_iter()
-                                .map(|image| ImageType::RawR32f { image })
+                                .map(|image| ImageType::RawRgba8u { image })
                                 .collect())
                         }
                     }
@@ -226,44 +221,44 @@ fn handle_allsky_file<F: ImageFormat>(
     allsky_tile_size: i32,
     texture_size: i32,
     tile_size: i32,
-) -> Result<Vec<ImageBuffer<F>>, JsValue> {
+) -> Result<impl Iterator<Item = ImageBuffer<F>>, JsValue> {
     let num_tiles_per_texture = (texture_size / tile_size) * (texture_size / tile_size);
     let num_tiles = num_tiles_per_texture * 12;
-    let mut tiles = Vec::with_capacity(num_tiles as usize);
 
     let num_allsky_tiles_per_tile = (tile_size / allsky_tile_size) * (tile_size / allsky_tile_size);
 
     let mut src_idx = 0;
-    for _ in 0..num_tiles {
-        let mut base_tile =
+    let tiles = (0..num_tiles)
+        .map(move |_| {
+            let mut base_tile =
             ImageBuffer::<F>::allocate(&<F as ImageFormat>::P::BLACK, tile_size, tile_size);
-        for idx_tile in 0..num_allsky_tiles_per_tile {
-            let (x, y) = crate::utils::unmortonize(idx_tile as u64);
-            let dx = x * (allsky_tile_size as u32);
-            let dy = y * (allsky_tile_size as u32);
+            for idx_tile in 0..num_allsky_tiles_per_tile {
+                let (x, y) = crate::utils::unmortonize(idx_tile as u64);
+                let dx = x * (allsky_tile_size as u32);
+                let dy = y * (allsky_tile_size as u32);
 
-            let sx = (src_idx % 27) * allsky_tile_size;
-            let sy = (src_idx / 27) * allsky_tile_size;
-            let s = ImageBufferView {
-                x: sx as i32,
-                y: sy as i32,
-                w: allsky_tile_size as i32,
-                h: allsky_tile_size as i32,
-            };
-            let d = ImageBufferView {
-                x: dx as i32,
-                y: dy as i32,
-                w: allsky_tile_size as i32,
-                h: allsky_tile_size as i32,
-            };
+                let sx = (src_idx % 27) * allsky_tile_size;
+                let sy = (src_idx / 27) * allsky_tile_size;
+                let s = ImageBufferView {
+                    x: sx as i32,
+                    y: sy as i32,
+                    w: allsky_tile_size as i32,
+                    h: allsky_tile_size as i32,
+                };
+                let d = ImageBufferView {
+                    x: dx as i32,
+                    y: dy as i32,
+                    w: allsky_tile_size as i32,
+                    h: allsky_tile_size as i32,
+                };
 
-            base_tile.tex_sub(&allsky, &s, &d);
+                base_tile.tex_sub(&allsky, &s, &d);
 
-            src_idx += 1;
-        }
+                src_idx += 1;
+            }
 
-        tiles.push(base_tile);
-    }
+            base_tile
+        });
 
     Ok(tiles)
 }
@@ -272,7 +267,7 @@ fn handle_allsky_fits<F: ImageFormat>(
     allsky_data: &[<<F as ImageFormat>::P as Pixel>::Item],
     tile_size: i32,
     texture_size: i32,
-) -> Result<Vec<ImageBuffer<F>>, JsValue> {
+) -> Result<impl Iterator<Item=ImageBuffer<F>>, JsValue> {
     let allsky_tile_size = std::cmp::min(tile_size, 64);
     let width_allsky_px = 27 * allsky_tile_size;
     let height_allsky_px = 29 * allsky_tile_size;
@@ -286,9 +281,8 @@ fn handle_allsky_fits<F: ImageFormat>(
 
     let allsky = ImageBuffer::<F>::new(reversed_rows_data, width_allsky_px, height_allsky_px);
 
-    let allsky_tiles = handle_allsky_file::<F>(allsky, allsky_tile_size, texture_size, tile_size)?
-        .into_iter()
-        .map(|image| {
+    let allsky_tiles_iter = handle_allsky_file::<F>(allsky, allsky_tile_size, texture_size, tile_size)?
+        .map(move |image| {
             // The GPU does a specific transformation on the UV
             // for FITS tiles
             // We must revert this to be compatible with this GPU transformation
@@ -298,10 +292,9 @@ fn handle_allsky_fits<F: ImageFormat>(
             }
 
             ImageBuffer::<F>::new(new_image_data, tile_size, tile_size)
-        })
-        .collect();
+        });
 
-    Ok(allsky_tiles)
+    Ok(allsky_tiles_iter)
 }
 
 use al_core::image::format::RGBA8U;

src/core/al-core/src/image/fits.rs
@@ -163,7 +163,7 @@ impl Image for Fits<'_> {
                 );
             }
             Data::F32(data) => {
-                let view = unsafe { R32F::view(&data) };
+                let view = unsafe { R8UI::view(&std::slice::from_raw_parts(data.as_ptr() as *const u8, data.len() * 4)) };
                 textures.tex_sub_image_3d_with_opt_array_buffer_view(
                     offset.x,
                     offset.y,

src/core/al-core/src/image/format.rs
@@ -152,21 +152,13 @@ impl ImageFormat for RGB32F {
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 pub struct R32F;
 impl ImageFormat for R32F {
-    type P = [f32; 1];
+    type P = [u8; 4];
 
-    const NUM_CHANNELS: usize = 1;
+    const NUM_CHANNELS: usize = 4;
 
-    #[cfg(feature = "webgl2")]
-    const FORMAT: u32 = WebGlRenderingCtx::RED as u32;
-    #[cfg(feature = "webgl1")]
-    const FORMAT: u32 = WebGlRenderingCtx::LUMINANCE as u32;
-
-    #[cfg(feature = "webgl2")]
-    const INTERNAL_FORMAT: i32 = WebGlRenderingCtx::R32F as i32;
-    #[cfg(feature = "webgl1")]
-    const INTERNAL_FORMAT: i32 = WebGlRenderingCtx::LUMINANCE as i32;
-
-    const TYPE: u32 = WebGlRenderingCtx::FLOAT;
+    const FORMAT: u32 = WebGlRenderingCtx::RGBA as u32;
+    const INTERNAL_FORMAT: i32 = WebGlRenderingCtx::RGBA8 as i32;
+    const TYPE: u32 = WebGlRenderingCtx::UNSIGNED_BYTE;
 
     const CHANNEL_TYPE: ChannelType = ChannelType::R32F;
 
@@ -174,41 +166,31 @@ impl ImageFormat for R32F {
         Ok(Bytes::Borrowed(raw_bytes))
     }
 
-    type ArrayBufferView = js_sys::Float32Array;
+    type ArrayBufferView = js_sys::Uint8Array;
 
     unsafe fn view(s: &[<Self::P as Pixel>::Item]) -> Self::ArrayBufferView {
         Self::ArrayBufferView::view(s)
     }
 }
 
-#[cfg(feature = "webgl2")]
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 pub struct R64F;
-#[cfg(feature = "webgl2")]
 impl ImageFormat for R64F {
-    type P = [f32; 1];
+    type P = [u8; 4];
 
-    const NUM_CHANNELS: usize = 1;
+    const NUM_CHANNELS: usize = 4;
 
-    #[cfg(feature = "webgl2")]
-    const FORMAT: u32 = WebGlRenderingCtx::RED as u32;
-    #[cfg(feature = "webgl1")]
-    const FORMAT: u32 = WebGlRenderingCtx::LUMINANCE as u32;
+    const FORMAT: u32 = WebGlRenderingCtx::RGBA as u32;
+    const INTERNAL_FORMAT: i32 = WebGlRenderingCtx::RGBA8 as i32;
+    const TYPE: u32 = WebGlRenderingCtx::UNSIGNED_BYTE;
 
-    #[cfg(feature = "webgl2")]
-    const INTERNAL_FORMAT: i32 = WebGlRenderingCtx::R32F as i32;
-    #[cfg(feature = "webgl1")]
-    const INTERNAL_FORMAT: i32 = WebGlRenderingCtx::LUMINANCE as i32;
-
-    const TYPE: u32 = WebGlRenderingCtx::FLOAT;
-
-    const CHANNEL_TYPE: ChannelType = ChannelType::R64F;
+    const CHANNEL_TYPE: ChannelType = ChannelType::R32F;
 
     fn decode(raw_bytes: &[u8]) -> Result<Bytes<'_>, &'static str> {
         Ok(Bytes::Borrowed(raw_bytes))
     }
 
-    type ArrayBufferView = js_sys::Float32Array;
+    type ArrayBufferView = js_sys::Uint8Array;
 
     unsafe fn view(s: &[<Self::P as Pixel>::Item]) -> Self::ArrayBufferView {
         Self::ArrayBufferView::view(s)
@@ -309,6 +291,18 @@ pub enum ChannelType {
     R32I,
 }
 
+impl ChannelType {
+    pub fn is_colored(&self) -> bool {
+        match self {
+            ChannelType::RGBA32F
+            | ChannelType::RGB32F
+            | ChannelType::RGBA8U
+            | ChannelType::RGB8U => true,
+            _ => false,
+        }
+    }
+}
+
 pub const NUM_CHANNELS: usize = 9;
 
 #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
@@ -327,12 +321,6 @@ impl ImageFormatType {
     }
 
     pub fn is_colored(&self) -> bool {
-        match self.channel {
-            ChannelType::RGBA32F
-            | ChannelType::RGB32F
-            | ChannelType::RGBA8U
-            | ChannelType::RGB8U => true,
-            _ => false,
-        }
+        self.channel.is_colored()
     }
 }

src/core/al-core/src/texture/pixel.rs
@@ -11,6 +11,7 @@ pub trait Pixel:
         + Copy
         + std::fmt::Debug
         + cgmath::Zero
+        + cgmath::One
         + std::cmp::PartialEq
         + crate::convert::Cast<f32>;
     type Container: ArrayBuffer<Item = Self::Item>;

src/core/src/renderable/hips/d2/mod.rs
@@ -75,13 +75,22 @@ pub fn get_raster_shader<'a>(
     shaders: &'a mut ShaderManager,
     config: &HiPSConfig,
 ) -> Result<&'a Shader, JsValue> {
-    if config.get_format().is_colored() && cmap.label() == "native" {
-        crate::shader::get_shader(
-            gl,
-            shaders,
-            "hips_rasterizer_raster.vert",
-            "hips_rasterizer_color.frag",
-        )
+    if config.get_format().is_colored() {
+        if cmap.label() == "native" {
+            crate::shader::get_shader(
+                gl,
+                shaders,
+                "hips_rasterizer_raster.vert",
+                "hips_rasterizer_color.frag",
+            )
+        } else {
+            crate::shader::get_shader(
+                gl,
+                shaders,
+                "hips_rasterizer_raster.vert",
+                "hips_rasterizer_color_to_colormap.frag",
+            )
+        }
     } else {
         if config.tex_storing_unsigned_int {
             crate::shader::get_shader(
@@ -115,13 +124,22 @@ pub fn get_raytracer_shader<'a>(
     config: &HiPSConfig,
 ) -> Result<&'a Shader, JsValue> {
     //let colored_hips = config.is_colored();
-    if config.get_format().is_colored() && cmap.label() == "native" {
-        crate::shader::get_shader(
-            gl,
-            shaders,
-            "hips_raytracer_raytracer.vert",
-            "hips_raytracer_color.frag",
-        )
+    if config.get_format().is_colored() {
+        if cmap.label() == "native" {
+            crate::shader::get_shader(
+                gl,
+                shaders,
+                "hips_raytracer_raytracer.vert",
+                "hips_raytracer_color.frag",
+            )
+        } else {
+            crate::shader::get_shader(
+                gl,
+                shaders,
+                "hips_raytracer_raytracer.vert",
+                "hips_raytracer_color_to_colormap.frag",
+            )
+        }
     } else {
         if config.tex_storing_unsigned_int {
             crate::shader::get_shader(

src/core/src/renderable/hips/d3/mod.rs
@@ -52,13 +52,22 @@ pub fn get_raster_shader<'a>(
     shaders: &'a mut ShaderManager,
     config: &HiPSConfig,
 ) -> Result<&'a Shader, JsValue> {
-    if config.get_format().is_colored() && cmap.label() == "native" {
-        crate::shader::get_shader(
-            gl,
-            shaders,
-            "hips3d_rasterizer_raster.vert",
-            "hips3d_rasterizer_color.frag",
-        )
+    if config.get_format().is_colored() {
+        if cmap.label() == "native" {
+            crate::shader::get_shader(
+                gl,
+                shaders,
+                "hips3d_rasterizer_raster.vert",
+                "hips3d_rasterizer_color.frag",
+            )
+        } else {
+            crate::shader::get_shader(
+                gl,
+                shaders,
+                "hips3d_rasterizer_raster.vert",
+                "hips3d_rasterizer_color_to_colormap.frag",
+            )
+        }
     } else {
         if config.tex_storing_unsigned_int {
             crate::shader::get_shader(

src/core/src/renderable/image/cuts.rs
@@ -1,23 +1,23 @@
 use std::cmp::Ordering;
 use std::ops::Range;
-pub fn first_and_last_percent<T>(slice: &mut [T], first_percent: i32, last_percent: i32) -> Range<T>
+pub fn first_and_last_percent<T>(slice: &mut [T], mut first_percent: i32, mut last_percent: i32) -> Range<T>
 where
     T: PartialOrd + Copy,
 {
+    if first_percent > last_percent {
+        std::mem::swap(&mut first_percent, &mut last_percent);
+    }
+
     let n = slice.len();
-    let first_pct_idx = ((first_percent as f32) * 0.01 * (n as f32)) as usize;
-    let last_pct_idx = ((last_percent as f32) * 0.01 * (n as f32)) as usize;
+    let i1 = ((first_percent as f32) * 0.01 * (n as f32)) as usize;
+    let i2 = ((last_percent as f32) * 0.01 * (n as f32)) as usize;
 
     let min_val = {
-        let (_, min_val, _) = slice.select_nth_unstable_by(first_pct_idx, |a, b| {
-            a.partial_cmp(b).unwrap_or(Ordering::Greater)
-        });
+        let (_, min_val, _) = slice.select_nth_unstable_by(i1, |a, b| a.partial_cmp(b).unwrap_or(Ordering::Greater));
         *min_val
     };
     let max_val = {
-        let (_, max_val, _) = slice.select_nth_unstable_by(last_pct_idx, |a, b| {
-            a.partial_cmp(b).unwrap_or(Ordering::Greater)
-        });
+        let (_, max_val, _) = slice.select_nth_unstable_by(i2, |a, b| a.partial_cmp(b).unwrap_or(Ordering::Greater));
         *max_val
     };
 

commit 312b9844d1022d226bd0bdc95adcba6a8fc1af99

Fix allsky fits display
Bug introduced in #254. Allsky is also downloaded if the tile_size is <= 256

src/core/src/downloader/request/allsky.rs
@@ -185,14 +185,24 @@ impl From<query::Allsky> for AllskyRequest {
                                 .collect())
                         }
                         InMemData::F32(data) => {
-                            let data = unsafe { std::slice::from_raw_parts(data.as_ptr() as *const u8, data.len() * 4) };
+                            let data = unsafe {
+                                std::slice::from_raw_parts(
+                                    data.as_ptr() as *const u8,
+                                    data.len() * 4,
+                                )
+                            };
                             Ok(handle_allsky_fits(&data, tile_size, texture_size)?
                                 .map(|image| ImageType::RawRgba8u { image })
                                 .collect())
                         }
                         InMemData::F64(data) => {
                             let data = data.iter().map(|v| *v as f32).collect::<Vec<_>>();
-                            let data = unsafe { std::slice::from_raw_parts(data.as_ptr() as *const u8, data.len() * 4) };
+                            let data = unsafe {
+                                std::slice::from_raw_parts(
+                                    data.as_ptr() as *const u8,
+                                    data.len() * 4,
+                                )
+                            };
 
                             Ok(handle_allsky_fits(&data, tile_size, texture_size)?
                                 .map(|image| ImageType::RawRgba8u { image })
@@ -228,71 +238,77 @@ fn handle_allsky_file<F: ImageFormat>(
     let num_allsky_tiles_per_tile = (tile_size / allsky_tile_size) * (tile_size / allsky_tile_size);
 
     let mut src_idx = 0;
-    let tiles = (0..num_tiles)
-        .map(move |_| {
-            let mut base_tile =
+    let tiles = (0..num_tiles).map(move |_| {
+        let mut base_tile =
             ImageBuffer::<F>::allocate(&<F as ImageFormat>::P::BLACK, tile_size, tile_size);
-            for idx_tile in 0..num_allsky_tiles_per_tile {
-                let (x, y) = crate::utils::unmortonize(idx_tile as u64);
-                let dx = x * (allsky_tile_size as u32);
-                let dy = y * (allsky_tile_size as u32);
+        for idx_tile in 0..num_allsky_tiles_per_tile {
+            let (x, y) = crate::utils::unmortonize(idx_tile as u64);
+            let dx = x * (allsky_tile_size as u32);
+            let dy = y * (allsky_tile_size as u32);
 
-                let sx = (src_idx % 27) * allsky_tile_size;
-                let sy = (src_idx / 27) * allsky_tile_size;
-                let s = ImageBufferView {
-                    x: sx as i32,
-                    y: sy as i32,
-                    w: allsky_tile_size as i32,
-                    h: allsky_tile_size as i32,
-                };
-                let d = ImageBufferView {
-                    x: dx as i32,
-                    y: dy as i32,
-                    w: allsky_tile_size as i32,
-                    h: allsky_tile_size as i32,
-                };
+            let sx = (src_idx % 27) * allsky_tile_size;
+            let sy = (src_idx / 27) * allsky_tile_size;
+            let s = ImageBufferView {
+                x: sx as i32,
+                y: sy as i32,
+                w: allsky_tile_size as i32,
+                h: allsky_tile_size as i32,
+            };
+            let d = ImageBufferView {
+                x: dx as i32,
+                y: dy as i32,
+                w: allsky_tile_size as i32,
+                h: allsky_tile_size as i32,
+            };
 
-                base_tile.tex_sub(&allsky, &s, &d);
+            base_tile.tex_sub(&allsky, &s, &d);
 
-                src_idx += 1;
-            }
+            src_idx += 1;
+        }
 
-            base_tile
-        });
+        base_tile
+    });
 
     Ok(tiles)
 }
 
 fn handle_allsky_fits<F: ImageFormat>(
     allsky_data: &[<<F as ImageFormat>::P as Pixel>::Item],
+
     tile_size: i32,
     texture_size: i32,
-) -> Result<impl Iterator<Item=ImageBuffer<F>>, JsValue> {
+) -> Result<impl Iterator<Item = ImageBuffer<F>>, JsValue> {
     let allsky_tile_size = std::cmp::min(tile_size, 64);
     let width_allsky_px = 27 * allsky_tile_size;
     let height_allsky_px = 29 * allsky_tile_size;
     // The fits image layout stores rows in reverse
     let reversed_rows_data = allsky_data
-        .chunks(width_allsky_px as usize)
+        .chunks(width_allsky_px as usize * F::NUM_CHANNELS)
         .rev()
         .flatten()
         .copied()
         .collect::<Vec<_>>();
-
     let allsky = ImageBuffer::<F>::new(reversed_rows_data, width_allsky_px, height_allsky_px);
 
-    let allsky_tiles_iter = handle_allsky_file::<F>(allsky, allsky_tile_size, texture_size, tile_size)?
-        .map(move |image| {
-            // The GPU does a specific transformation on the UV
-            // for FITS tiles
-            // We must revert this to be compatible with this GPU transformation
-            let mut new_image_data = Vec::with_capacity(tile_size as usize);
-            for c in image.get_data().chunks((tile_size * tile_size) as usize) {
-                new_image_data.extend(c.chunks(tile_size as usize).rev().flatten());
-            }
+    let allsky_tiles_iter =
+        handle_allsky_file::<F>(allsky, allsky_tile_size, texture_size, tile_size)?.map(
+            move |image| {
+                // The GPU does a specific transformation on the UV for FITS tiles
+                // We must revert this to be compatible with this GPU transformation
+                let new_image_data = image
+                    .get_data()
+                    .chunks((tile_size * tile_size) as usize * F::NUM_CHANNELS)
+                    .flat_map(|c| {
+                        c.chunks(tile_size as usize * F::NUM_CHANNELS)
+                            .rev()
+                            .flatten()
+                    })
+                    .cloned()
+                    .collect();
 
-            ImageBuffer::<F>::new(new_image_data, tile_size, tile_size)
-        });
+                ImageBuffer::<F>::new(new_image_data, tile_size, tile_size)
+            },
+        );
 
     Ok(allsky_tiles_iter)
 }

src/core/src/tile_fetcher.rs
@@ -2,10 +2,10 @@ use crate::downloader::{query, Downloader};
 use crate::time::{DeltaTime, Time};
 use crate::Abort;
 
+use al_api::moc::MOCOptions;
 use std::cell::RefCell;
 use std::collections::{HashMap, VecDeque};
 use std::rc::Rc;
-use al_api::moc::MOCOptions;
 
 const MAX_NUM_TILE_FETCHING: usize = 8;
 const MAX_QUERY_QUEUE_LENGTH: usize = 100;
@@ -214,7 +214,7 @@ impl TileFetcherQueue {
 
         let tile_size = cfg.get_tile_size();
         //Request the allsky for the small tile size or if base tiles are not available
-        if tile_size <= 128 || cfg.get_min_depth_tile() > 0 {
+        if tile_size <= 256 || cfg.get_min_depth_tile() > 0 {
             // Request the allsky
             downloader.borrow_mut().fetch(query::Allsky::new(
                 cfg,